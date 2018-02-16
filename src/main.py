c2ca1f50f084ebecc39b5075dde9530a118f4047
Ian Schweer <schweer@adobe.com>
2018-02-15 21:36:39 -0800

    Adds docker compose file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import requests
 
-text = requests.get("http://will_you_press_etl:5000/8893")
-print(text.text)
+for i in range(1,5):
+    text = requests.get("http://willyoupress_node{0}.will_you_press_etl:5000/8893".format(i))
+    print("node {0} test: {1}".format(i, text.text))
+
+# setup a data base
+# has the master give the data base the results
